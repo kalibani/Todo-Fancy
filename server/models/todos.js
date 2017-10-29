752fe5775420d68b367c8580c0ac954c7288978f
kalibani <kalibani.ka@gmail.com>
2017-10-29 11:23:57 +0700

    todo api done

---
+++ server/models/todos.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let todoSchema = new Schema({
+  member:{
+    type: Schema.Types.ObjectId, ref: 'dataUser'
+  },
+  description:{
+    type: String
+  },
+  date:{
+    type: Date
+  },
+  status:{
+    type: Boolean
+  }
+})
+
+let todo = mongoose.model('todo', todoSchema);
+
+class Todos {
+  static findAll(){
+    return new Promise(function(resolve, reject) {
+      todo.find().populate("member").then((dataTodo) => {
+        resolve(dataTodo)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+    });
+  }
+
+  static createTodo(body){
+    return new Promise(function(resolve, reject) {
+      var todos = {
+        member: body.member,
+        description: body.description,
+        date: Date.now(),
+        status: body.status
+      }
+      console.log(todos);
+      todo.create(todos).then(dataTodo)
+        resolve(dataTodo)
+      .catch((err) => {
+        reject(err)
+      })
+    });
+  }
+
+  static updateTodo(params, body) {
+    return new Promise(function(resolve, reject) {
+      var id = {
+        _id : params.id
+      }
+      var update = {
+        member: body.member,
+        description: body.description,
+        date: Date.now(),
+        status: body.status
+      }
+      todo.findByIdAndUpdate(id, update).then((data) => {
+        resolve(data)
+      })
+      .catch(err=>{
+        reject(err)
+      })
+    })
+  }
+
+  static deleteTodo(params){
+    return new Promise(function(resolve, reject) {
+      var id = {
+        _id : params.id
+      }
+      todo.deleteOne(id)
+    })
+  }
+}
+
+module.exports = Todos;
