e4f9692e2f7dd231dcc1064f2d40557ffd58d7f7
kalibani <kalibani.ka@gmail.com>
2017-10-31 17:38:59 +0700

    refactor API

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const todoCtrl = require('../controllers/todoCtrl');
 
 router.get('/', todoCtrl.findAll )
+router.get('/:id', todoCtrl.getTodobyId)
 router.post('/', todoCtrl.createTodo)
 router.put('/:id', todoCtrl.updateTodo )
 router.delete('/:id', todoCtrl.deleteTodo)
